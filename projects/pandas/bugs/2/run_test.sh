9a79714976d1ef7deec208f3083db0c429bef9f3
simshengqin <sqsim.2018@sis.smu.edu.sg>
2020-05-27 12:24:34 +0800

    Added 2nd bug for pandas

---
+++ projects/pandas/bugs/2/run_test.sh
@@ -0,0 +1,4 @@
+pytest pandas/tests/indexing/test_scalar.py::test_at_with_tuple_index_get 
+pytest pandas/tests/indexing/test_scalar.py::test_at_with_tuple_index_set
+pytest pandas/tests/indexing/test_scalar.py::test_multiindex_at_get
+pytest pandas/tests/indexing/test_scalar.py::test_multiindex_at_set
