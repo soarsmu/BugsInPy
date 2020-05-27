ff8bae3c3d792c74497e6d3c24ee79ed45519681
simshengqin <sqsim.2018@sis.smu.edu.sg>
2020-05-27 15:53:23 +0800

    Added 4th bug for pandas

---
+++ projects/pandas/bugs/4/run_test.sh
@@ -0,0 +1,2 @@
+pytest pandas/tests/series/methods/test_to_period.py::TestToPeriod::test_to_period_raises
+pytest pandas/tests/series/methods/test_to_timestamp.py::TestToTimestamp::test_to_timestamp_raises
